be7e53c71c5f1f16ec611a48c59f0d7d4072d022
gkiarie1 <gkaris24@gmail.com>
2024-01-15 10:25:39 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+var num1=31;
+var num2=2;
+var multiply = num1 * num2;
+
+
+var random= Math.floor(Math.random()*10);
+
+var num3=12;
+var num4=8;
+
+var mod=num3 % num4;
+
+
+var max=Math.max(5,10,15,20);
